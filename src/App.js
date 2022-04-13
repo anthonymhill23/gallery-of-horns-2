ef62673ee5434afce52fdf71ffd9a991e28ccd6c
Anthony Hill <anthonymhill23@gmail.com>
2022-04-13 17:28:59 -0400

    importing modal and header and footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
-import data from "./provided.json"
+import React, { Component } from 'react'
+import Header from './Header.js'
+import Main from './Main.js'
+import Footer from './Footer.js'
+import data from './provided.json'
+import SelectedBeast from './modal.js'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main 
-      data={data}
-      />
-      <Footer />
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false,
+      selectedBeast: {},
+      favorite: 0,
+      search: "",
+      hornSelected: ""
+    }
+  }
+
+  openModal = (selectedBeast, favorite) => {
+    this.setState({ showModal: true, selectedBeast: selectedBeast, favorite: favorite })
+  }
+
+  closeModal = _ => {
+    this.setState({ showModal: false })
+  }
+
+  onchange = (event) => {
+    const searching = event.target.value.toLowerCase();
+    this.setState({ search: searching });
+  }
+
+  onchangetwo = (event) => {
+    this.setState({ hornSelected: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header onchange={this.onchange} onchangetwo={this.onchangetwo} />
+        <Main data={data} modalTrue={this.openModal} searchData={this.state.search} hornSelected={this.state.hornSelected} />
+        <Footer />
+        <SelectedBeast showModal={this.state.showModal} closeModal={this.closeModal} selectedBeast={this.state.selectedBeast} favorite={this.state.favorite} />
+      </div>
+    )
+  }
 }
-
-export default App;
